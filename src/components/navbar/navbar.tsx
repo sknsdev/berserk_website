b4b7e7f1c32385e0f65ab2e79b7077132d1107a8
Alexey Vlasov <a-vlasov@webjox.ru>
2024-06-03 22:09:44 +0300

    routing fix

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -13,7 +13,7 @@ export const pageList = [
   {
     id: 1,
     name: 'Контакты',
-    path: '/contacts', // Обновлено для использования якорной ссылки
+    path: '/contact',
   },
 ];
 
@@ -23,8 +23,10 @@ const NavBar = () => {
       <div className="p-4 lp:p-2">
         <div className="w-full bg-[#4343432c] py-4 px-24 sm:px-8 rounded-lg">
           <div className="flex lp:flex-col lp:gap-5 flex-wrap items-center justify-between">
-            {/* <h1 className="text-3xl font-bold text-white lp:text-center">Берсерк ассистент</h1> */}
-            <Image src={logo} alt="logo" width={48} height={48} loading="lazy" />
+            <Link href={'/'} className='flex direction-row gap-2 align-center'>
+              <h1 className="text-3xl font-bold text-white lp:text-center">Берсерк ассистент</h1>
+              <Image src={logo} alt="logo" width={48} height={48} loading="lazy" />
+            </Link>
 
             <ul className="text-white items-center font-bold flex flex-wrap lp:flex-col gap-10 lp:gap-5">
               {pageList.map((el) => (
