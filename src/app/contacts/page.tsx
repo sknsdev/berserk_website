b1126d77d8d46a5e49f9178f813e57b114fa3f77
Ramazan G <pochtagrs@mail.ru>
2024-06-04 12:33:03 +0300

    form fix

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,12 +1,68 @@
-import ContactsClient from '@/features/contactsForm/contactsFormClient';
+'use client';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { FormField, formFields } from '@/models/form/formFields';
 
-const ContactsPage = () => {
+const Contacts: React.FC = () => {
+  const [formData, setFormData] = useState<{ [key: string]: string }>(() => {
+    const initialState: { [key: string]: string } = {};
+    formFields.forEach((field) => {
+      initialState[field.name] = '';
+    });
+    return initialState;
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log('Форма отправлена успешно');
+    } else {
+      console.error('Ошибка при отправке формы');
+    }
+  };
+
   return (
     <main className="bg-hero-pattern bg-center bg-cover relative w-full min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-center font-bold text-4xl text-white mb-20">Контакты</h1>
-      <ContactsClient />
+      <form onSubmit={handleSubmit} className="flex flex-col items-center px-6 bg-[#fad6ac5b] rounded-lg p-4">
+        {formFields.map((field: FormField) => (
+          <div key={field.name} className="mb-4 ">
+            <label htmlFor={field.name} className="block text-sm font-medium text-foreground">
+              {field.label}
+            </label>
+            <input
+              type="text"
+              id={field.name}
+              name={field.name}
+              value={formData[field.name]}
+              onChange={handleChange}
+              required
+              className="p-2 block sm:w-64 shadow-sm sm:text-sm border-gray-300 rounded-md w-[500px]"
+            />
+          </div>
+        ))}
+        <button type="submit" className="bg-mainColor px-4 py-2 font-bold hover:bg-foreground hover:duration-300 rounded-md text-white">
+          Отправить
+        </button>
+      </form>
     </main>
   );
 };
 
-export default ContactsPage;
+export default Contacts;
--- src/features/downloadButton/index.tsx
+++ src/features/downloadButton/index.tsx
@@ -4,42 +4,6 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { downloadLinks } from '@/models/downloadLinks';
 import { itemVariants, listVariant, menuVariants } from '@/utils/animatePressets';
-// import { itemVariants, listVariant, menuVariants } from '@/utils/animatePressets';
-
-// const menuVariants = {
-//   open: { rotate: 180, transition: { duration: 0.4 } },
-//   closed: { rotate: 0, transition: { duration: 0.4 } },
-// };
-
-// const itemVariants = {
-//   open: {
-//     opacity: 1,
-//     y: 0,
-//     transition: { type: 'spring', stiffness: 300, damping: 20 },
-//   },
-//   closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
-// };
-
-// const listVariants = {
-//   open: {
-//     clipPath: 'inset(0% 0% 0% 0% round 10px)',
-//     transition: {
-//       type: 'spring',
-//       bounce: 0,
-//       duration: 0.4,
-//       delayChildren: 0.3,
-//       staggerChildren: 0.1,
-//     },
-//   },
-//   closed: {
-//     clipPath: 'inset(10% 50% 90% 50% round 10px)',
-//     transition: {
-//       type: 'spring',
-//       bounce: 0,
-//       duration: 0.3,
-//     },
-//   },
-// };
 
 const DownloadButton = () => {
   const [isOpen, setIsOpen] = useState<Boolean>(false);
