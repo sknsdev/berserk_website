97de04ddd06bdc8a6ad3f4941c31fe67bbefda8b
Ramazan G <pochtagrs@mail.ru>
2024-06-26 14:26:36 +0300

    animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import phone from '@/../public/hero/phone.png';
 import { Marck_Script } from 'next/font/google';
 import Image from 'next/image';
@@ -21,7 +22,7 @@ const HomePage = () => {
         </ul>
       </header>
       <section className="relative flex flex-col bg-mainDark justify-center items-center min-h-screen max-h-[100dvh] p-8">
-        <div className="w-full h-[100dvh] flex flex-col items-center justify-center  bg-mainOrange ">
+        <div className="w-full h-[100dvh] flex flex-col items-center justify-center bg-mainOrange">
           <div>
             <div className="flex flex-col justify-center items-center">
               <motion.h1
@@ -38,18 +39,28 @@ const HomePage = () => {
               </motion.h1>
 
               <motion.h2
-                initial={{ y: -100, opacity: 0 }}
+                initial={{ y: 100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 1, ease: 'easeOut' }}
+                transition={{ duration: 1, delay: 0.5, ease: 'easeOut' }}
                 className={`${mark.className} text-mainRose text-[200px] absolute bottom-[200px]`}
               >
                 Ассистент
               </motion.h2>
             </div>
-            <p className="text-left max-w-[400px] text-sm text-white">Бесплатное мобильное приложение для ККИ “Берсерк” уже доступно в Google Store и Ru Store</p>
+
+            <motion.p
+              initial={{ x: '-100%', opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.2, type: 'spring' }}
+              className="text-left max-w-[400px] text-sm text-white"
+            >
+              Бесплатное мобильное приложение для ККИ “Берсерк” уже доступно в Google Store и Ru Store
+            </motion.p>
           </div>
         </div>
-        <Image src={phone} alt="phone" placeholder="empty" priority className="absolute w-[600px] bottom-0" />
+        <motion.div initial={{ opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.5, type: 'spring' }} className="absolute w-[600px] bottom-0">
+          <Image src={phone} alt="phone" placeholder="empty" priority />
+        </motion.div>
       </section>
     </main>
   );
