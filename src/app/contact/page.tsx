f833d852553fdb4c5ab59fd90f75daf253b2bacd
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2024-07-30 14:36:45 +0300

    renamed support to contact pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { ContactsFormClient } from '@/features/contactsForm/contactsFormClient';
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const Support = () => {
+const Contact = () => {
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.15, type: 'spring' }}>
       <ContactsFormClient />
@@ -11,4 +11,4 @@ const Support = () => {
   );
 };
 
-export default Support;
+export default Contact;
--- src/models/navLinks/index.tsx
+++ src/models/navLinks/index.tsx
@@ -9,6 +9,6 @@ export const pageList: IPageList[] = [
   {
     id: 1,
     name: 'Поддержка',
-    path: '/support',
+    path: '/contact',
   },
 ];
