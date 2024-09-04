d93babdabe770d832e8c237c39e7f38c43564f73
Alexey Vlasov <sketchrhc@yandex.ru>
2024-09-04 20:12:47 +0300

    metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/fc/header';
 import Footer from '@/components/fc/footer';
 import { PageTransitionLayout } from '@/features/pageTransition/layout';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 // const inter = Lora({ subsets: ['latin'] });
@@ -21,6 +22,22 @@ export default function RootLayout({
   return (
     <html lang="ru-RU">
       <body className={`${inter.className} bg-mainOrange`}>
+      <Script id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(98243631, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`
+            }
+          </Script>
         <Header />
         <PageTransitionLayout>
           <div className="w-full min-h-[100dvh] flex flex-col items-center justify-center">{children}</div>
--- src/components/fc/header/index.tsx
+++ src/components/fc/header/index.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
       <nav className="text-mainDark p-4 text-lg flex gap-4 justify-center">
         {pageList.map((link) => {
           return (
-            <Link key={link.id} href={`${link.path}`} className="hover:font-bold duration-300">
+            <Link key={link.id} href={`${link.path}`} className="hover:font-bold duration-300 text-center">
               {link.name}
             </Link>
           );
